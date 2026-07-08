0af16964d86dbbd40fc2fc7d997e6aaeea47bcaa
tyson_rottie <amirabdullahofficial06@gmail.com>
2026-07-08 18:07:52 -0400

    Need  to add animations to pieces when falling

---
+++ connect_4_180.py
@@ -0,0 +1,664 @@
+import time
+import pygame
+import sys
+import math
+import random
+from connect_4_ai import minimax
+from connect_4_ai import winning_move
+from connect_4_ai import get_valid_locations
+import numpy as np
+
+pygame.init()
+pygame.mixer.init()
+turn = 1
+win = 0
+space = 4
+color = (0,0,0)
+text = 'Connect 4'
+room = 0
+black = (0, 0, 0)
+white = (255, 255, 255)
+gold = (212, 175, 55)
+menu_color = (22, 163, 11)
+do_input = 1
+
+def create_board():
+    board = np.zeros((6, 7))
+    return board
+
+# if 0 just flip y, if 6 flip y and x
+h_flip = 0
+
+mini_grid = create_board()
+
+#window size
+win_x,win_y = 640, 640
+
+ 
+ #get scale
+screen = pygame.display.set_mode((win_x,win_y))
+pygame.display.set_caption("Connect 4")
+text = 'Connect 4'
+text_color = (255, 255, 255)
+
+
+
+scale_x = win_x/7
+scale_y = win_y/7
+
+font = pygame.font.SysFont('Arial', 30)
+text_surface = font.render(text, True, text_color)
+text_rect = text_surface.get_rect(center=(win_x/2, win_y/2))
+text_rect.center = (win_x/2, scale_y/2 - 20)
+
+text_rect_2 = text_surface.get_rect(center=(win_x/2, win_y/2))
+text_rect_2.center = (win_x/2, scale_y/2 + 20)
+
+b_place_x = []
+b_place_y = []
+
+p_place_x = []
+p_place_y = []
+
+
+f_b_place_x = []
+f_b_place_y = []
+
+f_p_place_x = []
+f_p_place_y = []
+
+next_flip = 6
+long = next_flip
+
+
+time_passed = 0
+running = True 
+
+grid_2 = [[0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ ]
+
+
+grid = [[0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ ]
+
+
+
+b_temp_x = []
+b_temp_y = []
+p_temp_x = []
+p_temp_y = []
+
+
+
+
+
+
+def pretty_print_board(board):
+    flipped_board = np.flipud(board)
+
+    print("\033[0;37;41m 0 \033[0;37;41m 1 \033[0;37;41m 2 \033[0;37;41m 3 \033[0;37;41m 4 \033[0;37;41m 5 \033[0;37;41m 6 \033[0m")
+    for i in flipped_board:
+        row_str = ""
+
+        for j in i:
+            if j == 1:
+                #print(yellow)
+                row_str +="\033[0;37;43m 1 "
+            elif j ==2:
+                row_str +="\033[0;37;44m 2 "
+            else:
+                #print black
+                row_str +="\033[0;37;45m   "
+
+        print(row_str+"\033[0m")
+
+
+start_ticks = pygame.time.get_ticks()
+
+
+
+
+
+
+
+
+
+def reset():
+    global grid, b_place_x, b_place_y, p_place_x, p_place_y, turn, win, grid_2, next_flip
+    grid_reset(grid)
+    grid_reset(grid_2)
+    b_place_x = []
+    b_place_y = []
+    
+    b_temp_x = []
+    b_temp_y = []
+    turn = 1
+    win = 0
+    p_place_x = []
+    p_place_y = []
+    
+    p_temp_x = []
+    p_temp_y = []
+    next_flip = long
+    
+def grid_reset(which):
+    global grid, b_place_x, b_place_y, p_place_x, p_place_y, turn, win, grid_2
+    for l in range(6):
+        for i in range(7):
+            which[l][i] = 0
+
+
+def piece_fall2(piece, collum):
+    global text, turn, color, running, win, grid_2
+
+    row = 0
+
+    while row < 6:
+        if grid_2[row][collum] == 0:
+            if row == 5:
+                        grid_2[row][collum] = piece
+
+                        return row + 1
+  
+
+                        row = 7
+                    
+            else:
+                row += 1
+                
+        else: 
+            grid_2[row - 1][collum] = piece
+
+            return (row )
+
+
+
+            row = 7
+#make peices fall
+def piece_fall(piece, collum):
+    global text, turn, color, running, win
+
+    row = 0
+
+    while row < 6:
+        if grid[row][collum] == 0:
+            if row == 5:
+                        grid[row][collum] = piece
+
+                        return row + 1
+  
+
+                        row = 7
+                    
+            else:
+                row += 1
+                
+        else: 
+            grid[row - 1][collum] = piece
+
+            return (row )
+
+
+
+            row = 7
+
+#detect win
+def win_con():
+    #check if i - i+3 is equal to 1 blue wins if equal to 16 pink wins ``
+    #max i =3 3 x 
+    #max i = 2 y
+    global text, win
+    for k in range(6):
+        for i in range(4):
+            if math.prod(grid[k][i:i + 4]) == 1:
+                print("blue wins H")
+                text = 'Blue Wins'
+                win = 1 
+                                         
+                    
+            elif math.prod(grid[k][i:i + 4]) == 16:
+                print("Pink wins H")
+                text = 'Pink Wins'
+                win = 1
+                    
+        #vertical wins
+
+    for l in range(7):
+        for i in range(3):
+            vertical = [grid[i][l],  grid[i + 1][l], grid[i + 2][l], grid[i + 3][l]]
+            if math.prod(vertical) == 1:
+                print("blue wins V")
+                text = 'Blue Wins'
+                win = 1                    
+            elif math.prod(vertical) == 16:
+                print("Pink wins V")
+                text = 'Pink Wins'
+                win = 1
+        #D wins 
+    for l in range(4):
+        for i in range(3):
+            dia = [grid[i][l],  grid[i + 1][l + 1], grid[i + 2][l + 2], grid[i + 3][l + 3]]
+            if math.prod(dia) == 1:
+                print("blue wins D")
+                text = 'Blue Wins'
+                win = 1                    
+            elif math.prod(dia) == 16:
+                print("Pink wins D")
+                text = 'Pink Wins'
+                win = 1
+                
+    for l in range(4):
+        for i in range(3,6):
+            dia2 = [grid[i][l],  grid[i - 1][l + 1], grid[i - 2][l + 2], grid[i - 3][l + 3]]
+            if math.prod(dia2) == 1:
+                print("blue wins D")
+                text = 'Blue Wins'
+                win = 1                    
+            elif math.prod(dia2) == 16:
+                print("Pink wins D")
+                text = 'Pink Wins'
+                win = 1
+
+    #not working, less than 1% chance of tie so
+    if math.prod(grid[0])> 0:
+        text = 'Tie'
+        
+    return win 
+
+
+#load images 
+def load_images(pic, buffer):
+    img = pygame.image.load('assets\\' + pic).convert_alpha()
+    img = pygame.transform.scale(img, (scale_x - buffer , scale_y - buffer))    
+
+    return img 
+ 
+ 
+border_img = load_images('4_border.png', 0)
+blue_img = load_images('4_blue.png', space)   
+pink_img = load_images('4_pink.png', space)
+
+border_x = []
+border_y = []
+
+#tie detection
+ 
+
+
+for i in range(7):
+    for k in range(7):
+        border_x.append(i * scale_x)
+        border_y.append(k * scale_y)
+
+
+fall_sound = pygame.mixer.Sound('assets\\fall_sound.wav')
+win = 0
+
+
+
+
+def main_game_player(events):
+    global text, turn, color, running, win, do_input, b_place_x, b_place_y, next_flip, long, b_temp_x, b_temp_y, p_temp_x, p_temp_y
+
+
+    until_text = "Turns until next flip: " + str(next_flip)
+    until_next = font.render(until_text, True, text_color)
+
+
+
+
+
+    if next_flip == 0:
+        do_input = 0
+        time.sleep(0.05)
+        
+        grid_flip()
+        next_flip = random.randint(1, 6)
+        #next_flip = long
+        
+    if do_input == 0:
+        do_input = 1
+        return
+    if math.prod(grid[0])> 0:
+        text = 'Tie'
+    text_surface = font.render(text, True, text_color)
+    screen.fill((204,196,18))
+    pygame.draw.rect(screen, color, (0, 0, win_x, scale_y), 0)
+    
+    menu_button(25, 12.5, 50, 25, "Back", 0, black, white, events, white)
+    
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_x = mouse_pos[0]
+
+    
+    screen.blit(text_surface, text_rect)
+    screen.blit(until_next, text_rect_2)
+
+    
+    #switch between turns
+    if win == 0:
+        if turn == 1: 
+            target = 1 #b
+            text = 'Blue Turn'
+            color = (0, 56, 168)
+            #timer(current_time, time_passed)
+
+        else: 
+            text = 'Pink Turn'
+            target = 2
+            color = (214, 2, 112)
+    #place images on screen    
+    for i in range(len(border_x)):
+        screen.blit(border_img, (border_x[i], border_y[i] + scale_x))
+    if len(b_place_y) > 0:
+        for i in range(len(b_place_y)):
+            screen.blit(blue_img, (b_place_x[i], b_place_y[i]))
+    for i in range(len(p_place_y)):
+        screen.blit(pink_img, (p_place_x[i] + 2, p_place_y[i]))
+    #animate falling 
+    for i in range(len(b_temp_y)):
+        screen.blit(blue_img, (b_temp_x[i], b_temp_y[i]))
+    for i in range(len(p_temp_y)):
+        screen.blit(pink_img, (p_temp_x[i], p_temp_y[i]))  
+        
+    pygame.display.flip()
+
+
+
+                
+    
+    
+    for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if win == 0:
+                    if event.button == 1:
+
+
+                        #detrime what column mouse is in
+                        collum = float(mouse_x/scale_x)
+                        collum = math.floor(collum)
+                        
+                        #place piece in correct column
+                        row = piece_fall(target, collum)
+
+                        place_x = collum * scale_x 
+                        place_y = row * scale_y 
+                        if row > 0:
+                            
+                            fall_sound.play()
+                            
+                            if turn == 0:
+                                
+                                
+                                
+                                p_place_x.append(place_x)
+                                p_place_y.append(place_y)
+                                f_p_place_x.append(abs(place_x - win_x))
+                                f_p_place_y.append(abs(place_y - (win_y - scale_y)))
+                                turn = 1
+                            else: 
+                                b_place_x.append(place_x)
+                                b_place_y.append(place_y)
+                                f_p_place_x.append(abs(place_x - win_x))
+                                f_p_place_y.append(abs(place_y - (win_y - scale_y)))
+                                turn = 0
+                            next_flip -= 1
+                        win = win_con()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_e:
+                    #print(grid)
+                    pretty_print_board(grid)
+                    print(math.prod(grid[0][0:9]))
+                if event.key == pygame.K_g:
+                    print("no more debugging for you")
+   
+                    
+
+
+def main_game_ai(events, depth):
+    global text, turn, color, running, win, do_input, b_place_x, b_place_y, next_flip, long
+
+
+    until_text = "Turns until next flip: " + str(next_flip)
+    until_next = font.render(until_text, True, text_color)
+
+
+
+    if next_flip == 0:
+        do_input = 0
+        time.sleep(0.05)
+        
+        grid_flip()
+        next_flip = random.randint(1, 6)
+        #next_flip = long
+        
+    if do_input == 0:
+        do_input = 1
+        return
+    if math.prod(grid[0])> 0:
+        text = 'Tie'
+    text_surface = font.render(text, True, text_color)
+    screen.fill((204,196,18))
+    pygame.draw.rect(screen, color, (0, 0, win_x, scale_y), 0)
+    
+    menu_button(25, 12.5, 50, 25, "Back", 0, black, white, events, white)
+    
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_x = mouse_pos[0]
+
+    
+    screen.blit(text_surface, text_rect)
+    screen.blit(until_next, text_rect_2)
+
+    
+    #switch between turns
+    if win == 0:
+        if turn == 1: 
+            target = 1 #b
+            text = 'Blue Turn'
+            color = (0, 56, 168)
+            #timer(current_time, time_passed)
+
+        else: 
+            text = 'Pink Turn'
+            target = 2
+            color = (214, 2, 112)
+            matrix = np.array(grid[::-1])
+            collumn = minimax(matrix, depth,  -math.inf, math.inf, True)   
+            score = collumn[1]
+            collumn = collumn[0]
+            print(collumn) 
+            if collumn == None or score == 0:
+                collumn = random.randint(0, 6)
+            row = piece_fall(target, collumn)
+            place_x = collumn * scale_x 
+            place_y = row * scale_y 
+            
+            p_place_x.append(place_x)
+            
+            p_place_y.append(place_y)
+            win = win_con()    
+            turn = 1
+
+    #place images on screen    
+    for i in range(len(border_x)):
+        screen.blit(border_img, (border_x[i], border_y[i] + scale_x))
+    if len(b_place_y) > 0:
+        for i in range(len(b_place_y)):
+            screen.blit(blue_img, (b_place_x[i], b_place_y[i]))
+    for i in range(len(p_place_y)):
+        screen.blit(pink_img, (p_place_x[i], p_place_y[i]))
+
+        
+    pygame.display.flip()
+
+
+
+                
+    
+    
+    for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if win == 0:
+                    if event.button == 1:
+
+
+                        #detrime what column mouse is in
+                        collum = float(mouse_x/scale_x)
+                        collum = math.floor(collum)
+                        
+                        #place piece in correct column
+                        row = piece_fall(target, collum)
+                        place_x = collum * scale_x 
+                        place_y = row * scale_y 
+                        if row > 0:
+                            
+                            fall_sound.play()
+                            
+                            if turn == 0:
+                                
+                                
+                                
+
+                                turn = 1
+                            else: 
+                                b_place_x.append(place_x)
+                                b_place_y.append(place_y)
+                                f_p_place_x.append(abs(place_x - win_x))
+                                f_p_place_y.append(abs(place_y - (win_y - scale_y)))
+                                turn = 0
+                            next_flip -= 1
+                        win = win_con()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_e:
+                    #print(grid)
+                    matrix = matrix = np.array(grid[::-1])
+                    pretty_print_board(matrix)
+                    print(math.prod(grid[0][0:9]))
+                if event.key == pygame.K_g:
+                    matrix = matrix = np.array(grid[::-1])
+                    print(get_valid_locations(matrix))
+   
+                    
+def main_menu(events):
+    
+    global text, turn, color, running, win, screen, do_input, grid, grid_start
+    if do_input == 0:
+        do_input = 1
+        return
+    screen.fill(menu_color)
+    menu_button(win_x/2, win_y/3, 200, 100, "Player Vs Player", 1, black, white, events, white)
+    menu_button(win_x/2, win_y/3 + 150, 200, 100, "Player Vs AI", 2, black, white, events, white)
+    menu_button(win_x/2, win_y/3 + 300, 200, 100, "Reset", 6, black, white, events, white)
+
+    pygame.display.flip()
+    
+def main_menu_dif(events):
+
+    global text, turn, color, running, win, screen, do_input
+    
+    if do_input == 0:
+        do_input = 1
+        return
+    screen.fill(menu_color)
+    menu_button(50, 25, 100, 50, "Back", 0, black, white, events, white)
+    menu_button(win_x/2, win_y/3, 200, 100, "Easy", 3, black, white, events, white)
+    menu_button(win_x/2, win_y/3 + 150, 200, 100, "Normal", 4, black, white, events, white)
+    menu_button(win_x/2, win_y/3 + 300, 200, 100, "Hard", 5, black, white, events, white)
+    pygame.display.flip()
+
+
+def menu_button(pos_x, pos_y, width, height, b_text, room_num, back_color, t_color, events, border_color):
+    global room, running, do_input   
+    cen_x = width/2
+    cen_y = height/2
+    new_pos_x = pos_x - cen_x
+    new_pos_y = pos_y - cen_y
+    pygame.draw.rect(screen, back_color, (new_pos_x, new_pos_y, width, height), 0)
+    pygame.draw.rect(screen, border_color, (new_pos_x, new_pos_y, width, height), round(width * 0.05), 2)
+    text_surface = font.render(b_text, True, t_color)
+
+    t_width, t_height = text_surface.get_size()
+    scale_factor = min(width / t_width, height/ t_height)
+    new_width = int((t_width * scale_factor) * 0.8)
+    new_height = int((t_height * scale_factor) * 0.8)
+    text_surface = pygame.transform.smoothscale(text_surface, (new_width, new_height))
+    text_rect = text_surface.get_rect(center=(new_pos_x + cen_x, new_pos_y + cen_y))
+    text_rect.center = (new_pos_x + cen_x, new_pos_y + cen_y)
+    screen.blit(text_surface, text_rect)
+    
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_x = mouse_pos[0]
+    mouse_y = mouse_pos[1]
+    for event in events:
+        if event.type == pygame.MOUSEBUTTONDOWN and do_input == 1:
+            if event.button == 1:
+                if mouse_x >= new_pos_x and mouse_x <= new_pos_x + width and mouse_y >= new_pos_y and mouse_y <= new_pos_y + height:
+                        
+                        room = room_num
+                        do_input = 0
+        if event.type == pygame.QUIT:
+            running = False
+
+
+def grid_flip():
+    global grid, b_place_x, b_place_y, p_place_x, p_place_y, grid_2
+    b_place_x = []
+    b_place_y = []
+    
+    p_place_x = []
+    p_place_y = []
+    for i in range(6, -1, -1):
+        for l in range(6): 
+            if grid[l][i] == 1:
+                row_2 = piece_fall2(1, abs(h_flip - i)) 
+                b_place_x.append(abs((h_flip - i)) * scale_x)
+                b_place_y.append(row_2 * scale_y)
+            elif grid[l][i] == 2:
+                row_2 = piece_fall2(2, abs(h_flip- i)) 
+                p_place_x.append(abs((h_flip - i)) * scale_x)
+                p_place_y.append(row_2 * scale_y)
+  
+    for l in range(6):
+        for i in range(7):
+            grid[l][i] = grid_2[l][i]
+    grid_reset(grid_2)
+    win_con()
+
+while running:
+    events = pygame.event.get()
+    
+    if room == 0:
+        main_menu(events)
+    if room == 1:
+        main_game_player(events)
+    if room == 2:
+        main_menu_dif(events)
+    if room == 3:
+        main_game_ai(events, 2)
+    if room == 4:
+        main_game_ai(events, 5)
+    if room == 5:
+        main_game_ai(events, 8)
+    if room == 6:
+        #rest grid
+        reset()
+        print("reset")
+        room = 0
+pygame.QUIT 
+
+
